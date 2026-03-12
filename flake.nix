db842608b0b1ba0faac79293c48a7f1fdea468dc
jpereiratrindade <jpereiratrindade@gmail.com>
2026-03-12 15:22:40 -0300

    Add Nix development shell (flake.nix)

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "LabGestao development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            SDL2
+            libGL
+          ];
+
+          shellHook = ''
+            echo "🚀 LabGestao Development Environment"
+            echo "Dependencies: SDL2, OpenGL, CMake"
+            echo "Usage: cmake -B build && cmake --build build"
+          '';
+        };
+      }
+    );
+}
