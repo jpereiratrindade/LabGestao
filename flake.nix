8450d613387ce312d61250f23924b5719b072b7a
jpereiratrindade <jpereiratrindade@gmail.com>
2026-03-12 15:25:14 -0300

    Switch to stable nixos-24.11 channel for better reliability

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "LabGestao development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     utils.url = "github:numtide/flake-utils";
   };
 
